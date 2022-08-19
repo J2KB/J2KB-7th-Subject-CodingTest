d727dec9ba486ce651d1bec9b30e6964f35a47f8
jundoopop <jundolpop@gmail.com>
2022-08-20 01:11:12 +0900

    권준혁 1주차 - 풀이 설명하는 주석 추가

--- KwonJoonhyeok/python/leetcode/AddBinary67.py
+++ KwonJoonhyeok/python/leetcode/AddBinary67.py
@@ -7,22 +7,28 @@ class Solution:
         a_decimal = 0
         b_decimal = 0
 
+        # a 2진수 -> 10진수로 변경
         cnt = 0
         for bin_element in a:
             if bin_element == '1':
                 a_decimal += 2 ** (len(a) - 1 - cnt)
             cnt += 1
 
+        # b 2진수 -> 10진수
         cnt = 0
         for bin_element in b:
             if bin_element == '1':
                 b_decimal += 2 ** (len(b) - 1 - cnt)
             cnt += 1
 
+        # 10진수화한 a, b 더하기
         sum_decimal = a_decimal + b_decimal
+
+        # 0일 때
         if sum_decimal == 0:
             return '0'
 
+        # 0이 아닐 때
         else:
             result = ''
             while sum_decimal > 0:
