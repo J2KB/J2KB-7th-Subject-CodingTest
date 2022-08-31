2acf82c2e53aad5272112533ff2129e82801a689
heesu40 <heesu40@hanmail.net>
2022-08-31 16:05:18 +0900

    20220831

--- LeeHeesu/3주차/3.py
+++ LeeHeesu/3주차/3.py
@@ -7,4 +7,23 @@ arr = []
 a,b  = map(int, input().split())
 for i in range(a):
     arr.append(int(input()))
-print(arr)
+
+
+# 단순하게 큰 수로만 나누면 안되고 보텀업 방식 이용
+#10000개이므로
+d = [10001] * (b +1)
+d[0] = 0
+for i in range(a):
+    for j in range(arr[i], b+1):
+        if d[j - arr[i]] != 10001:
+            d[j] = min(d[j], d[j-arr[i]]+1)
+
+if d[b] == 10001 :
+    print(-1)
+else :
+    print(d[b])        
+
+
+        
+       
+    
