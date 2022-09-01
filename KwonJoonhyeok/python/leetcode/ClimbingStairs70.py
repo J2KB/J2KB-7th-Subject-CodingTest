c97378aef89158a6a146b883b6fc137fc2e02323
jundoopop <jundolpop@gmail.com>
2022-09-01 22:07:11 +0900

    권준혁 3주차  - 로또 최고, 최저 순위 -  프로그래머스

--- KwonJoonhyeok/python/leetcode/ClimbingStairs70.py
+++ KwonJoonhyeok/python/leetcode/ClimbingStairs70.py
@@ -1,17 +1,21 @@
-import math
+# 1 stair = 1
+# 2 stairs = 2
+# 3 stairs = 3
+# 4 stairs = 5
+# 5 stairs = 8
+# 6 stairs = 13
+# (n) stairs + (n+1) stairs = (n+2) stairs
 
 
+# 귀납적 추론을 통해 얻은 점화식으로
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # integer to return
-        result = 0
+        fibonacci = [0, 1, 2]
+        if n < 3:
+            return n
 
-        # how many 2 steps
-        distinct_by_two = n // 2
+        else:
+            for i in range(3, n + 1):
+                fibonacci.append(fibonacci[i - 2] + fibonacci[i - 1])
 
-        # add cases
-        for i in range(distinct_by_two + 1):
-            number_of_climbing = n - i
-            result += math.comb(n - i, i)
-
-        return result
+        return fibonacci[n]
