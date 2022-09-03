e8085d8acb853914b3e14fa66c30e980118ab83e
jundoopop <jundolpop@gmail.com>
2022-09-03 12:02:21 +0900

    복구git add .!

--- KwonJoonhyeok/python/leetcode/LengthofLastWord58.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-
-        # 각 단어 split, 그 중에서 맨 마지막 단어 반환
-        return len(s.split(' ')[-1])
-
-# https://leetcode.com/problems/length-of-last-word/submissions/
